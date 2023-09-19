958a1eae35ee8d07edc8dd7cd0c97d45f7a20db5
Ines Mauget <ines.mauget@etu.univ-poitiers.fr>
2023-09-19 18:12:03 +0200

    Ajout sous titre

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function changerTitre() {
     title.style.color = 'red';
 }
 
+const h2 = document.createElement("h2");
+h2.id = "sous-titre";
+h2.innerText = "Welcome to the javascript world";
+document.querySelector("h1").after(h2);
