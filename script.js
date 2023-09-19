ac8b9336d91233078c3134b396310440aca89408
Ines Mauget <ines.mauget@etu.univ-poitiers.fr>
2023-09-19 17:57:55 +0200

    Modifie le titre et la couleur avec JS
    Améliore la gestion du titre

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello world!!!")
+
+
+changerTitre();
+
+function changerTitre() {
+    const title = document.getElementById('title');
+    title.innerText += ' from JS';
+    title.style.color = 'red';
+}
+
